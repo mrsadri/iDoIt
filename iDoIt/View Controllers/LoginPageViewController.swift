f7910a1bb179f99a758b38a8af421b27c91ce27d
MSadri <sadri.masih@gmail.com>
2018-12-03 17:20:37 +0330

    I hate AutoLayout ... :(((((((((((((((((((((((((((((((((( hate you

--- iDoIt/View Controllers/LoginPageViewController.swift
+++ iDoIt/View Controllers/LoginPageViewController.swift
@@ -12,26 +12,18 @@ class LoginPageViewController: UIViewController, UITextFieldDelegate{
 
     @IBOutlet weak var blueBack: UIImageView!
     @IBOutlet weak var loginButton: UIButton!
-    @IBOutlet weak var panelBackGround: UIImageView!
+    @IBOutlet weak var loginPanel: UIImageView!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var pTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Do any additional setup after loading the view.
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(true)
-    }
-    
-    override func viewWillLayoutSubviews() {
         self.setupLayout()
         self.emailTextField.delegate = self
         self.pTextField.delegate = self
-        
+        // Do any additional setup after loading the view.
     }
+    
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         // Try to find next responder
         if let nextField = textField.superview?.viewWithTag(textField.tag + 1) as? UITextField {
@@ -46,13 +38,6 @@ class LoginPageViewController: UIViewController, UITextFieldDelegate{
         
     }
     
-    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
-        coordinator.animate(alongsideTransition: { (_) in
-
-        }) { (_) in
-            //
-        }
-    }
     
     func setupLayout() {
         blueBack.translatesAutoresizingMaskIntoConstraints = false
@@ -74,29 +59,24 @@ class LoginPageViewController: UIViewController, UITextFieldDelegate{
         loginButton.leadingAnchor.constraint(equalTo: blueBack.leadingAnchor, constant: 35 * modifyRate).isActive = true
         loginButton.heightAnchor.constraint(equalTo: loginButton.widthAnchor, multiplier: 53/286 ).isActive = true
 
-        panelBackGround.translatesAutoresizingMaskIntoConstraints = false
-        panelBackGround.bottomAnchor.constraint(equalTo: loginButton.topAnchor, constant: -10 * modifyRate).isActive = true
-        panelBackGround.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
-        panelBackGround.leadingAnchor.constraint(equalTo: blueBack.leadingAnchor, constant: 38 * modifyRate).isActive = true
-        panelBackGround.heightAnchor.constraint(equalTo: panelBackGround.widthAnchor, multiplier: 84/279).isActive = true
+        loginPanel.translatesAutoresizingMaskIntoConstraints = false
+        loginPanel.bottomAnchor.constraint(equalTo: loginButton.topAnchor, constant: -10 * modifyRate).isActive = true
+        loginPanel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        loginPanel.leadingAnchor.constraint(equalTo: blueBack.leadingAnchor, constant: 38 * modifyRate).isActive = true
+        loginPanel.heightAnchor.constraint(equalTo: loginPanel.widthAnchor, multiplier: 84/279).isActive = true
         
         pTextField.translatesAutoresizingMaskIntoConstraints = false
-        pTextField.bottomAnchor.constraint(equalTo: panelBackGround.bottomAnchor, constant: -12 * modifyRate).isActive = true
-        pTextField.leadingAnchor.constraint(equalTo: panelBackGround.leadingAnchor, constant: 100 * modifyRate).isActive = true
+        pTextField.bottomAnchor.constraint(equalTo: loginPanel.bottomAnchor, constant: -12 * modifyRate).isActive = true
+        pTextField.leadingAnchor.constraint(equalTo: loginPanel.leadingAnchor, constant: 100 * modifyRate).isActive = true
         pTextField.heightAnchor.constraint(equalToConstant: 20).isActive = true
-        pTextField.trailingAnchor.constraint(equalTo: panelBackGround.trailingAnchor, constant: -14).isActive = true
+        pTextField.trailingAnchor.constraint(equalTo: loginPanel.trailingAnchor, constant: -14).isActive = true
         
         emailTextField.translatesAutoresizingMaskIntoConstraints = false
-        emailTextField.topAnchor.constraint(equalTo: panelBackGround.topAnchor, constant: 14 * modifyRate).isActive = true
-        emailTextField.leadingAnchor.constraint(equalTo: panelBackGround.leadingAnchor, constant: 100 * modifyRate).isActive = true
+        emailTextField.topAnchor.constraint(equalTo: loginPanel.topAnchor, constant: 14 * modifyRate).isActive = true
+        emailTextField.leadingAnchor.constraint(equalTo: loginPanel.leadingAnchor, constant: 100 * modifyRate).isActive = true
         emailTextField.heightAnchor.constraint(equalToConstant: 20).isActive = true
-        emailTextField.trailingAnchor.constraint(equalTo: panelBackGround.trailingAnchor, constant: -14).isActive = true
-        
+        emailTextField.trailingAnchor.constraint(equalTo: loginPanel.trailingAnchor, constant: -14).isActive = true
 
-        
-    
-        
-        
     }
     
 
---
+++ iDoIt/View Controllers/RegisterPageViewController.swift
@@ -0,0 +1,84 @@
+//
+//  RegisterPageViewController.swift
+//  iDoIt
+//
+//  Created by MSadri on 12/3/18.
+//  Copyright © 2018 MSadri. All rights reserved.
+//
+
+import UIKit
+
+class RegisterPageViewController: UIViewController, UITextFieldDelegate{
+    
+    
+    @IBOutlet weak var blueBack: UIImageView!
+    @IBOutlet weak var registerPanel: UIImageView!
+    @IBOutlet weak var registerButton: UIButton!
+    @IBOutlet weak var stackViewM: UIStackView!
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var pTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setupLayout()
+        self.firstNameTextField.delegate = self
+        self.lastNameTextField.delegate = self
+        self.emailTextField.delegate = self
+        self.pTextField.delegate = self
+       // --
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Try to find next responder
+        if let nextField = textField.superview?.viewWithTag(textField.tag + 1) as? UITextField {
+            nextField.becomeFirstResponder()
+        } else {
+            // Not found, so remove keyboard.
+            textField.resignFirstResponder()
+            print("Register Action.........")
+        }
+        // Do not add a line break
+        return false
+    }
+    
+    
+    
+    func setupLayout() {
+        blueBack.translatesAutoresizingMaskIntoConstraints = false
+        blueBack.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 10).isActive = true
+        blueBack.leadingAnchor.constraint(greaterThanOrEqualTo: self.view.safeAreaLayoutGuide.leadingAnchor , constant: 5).isActive = true
+        blueBack.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        if Int(view.bounds.size.width ) - 10 < 367 {
+            blueBack.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor , constant: 5).isActive = true
+        } else {
+            let i = 0.5 * (self.view.bounds.width - 367)
+            blueBack.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor , constant: i).isActive = true
+        }
+        blueBack.heightAnchor.constraint(equalTo: blueBack.widthAnchor, multiplier: 394/367 ).isActive = true
+        
+        let modifyRate = self.blueBack.bounds.width / 367
+        registerButton.translatesAutoresizingMaskIntoConstraints = false
+        registerButton.bottomAnchor.constraint(equalTo: blueBack.bottomAnchor, constant: -30 * modifyRate).isActive = true
+        registerButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        registerButton.leadingAnchor.constraint(equalTo: blueBack.leadingAnchor, constant: 35 * modifyRate).isActive = true
+        registerButton.heightAnchor.constraint(equalTo: registerButton.widthAnchor, multiplier: 53/286 ).isActive = true
+
+        registerPanel.translatesAutoresizingMaskIntoConstraints = false
+        registerPanel.bottomAnchor.constraint(equalTo: registerButton.topAnchor, constant: -10 * modifyRate).isActive = true
+        registerPanel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        registerPanel.leadingAnchor.constraint(equalTo: blueBack.leadingAnchor, constant: 38 * modifyRate).isActive = true
+        registerPanel.heightAnchor.constraint(equalTo: registerPanel.widthAnchor, multiplier: 160/279).isActive = true
+        
+        stackViewM.translatesAutoresizingMaskIntoConstraints = false
+        stackViewM.bottomAnchor.constraint(equalTo: registerPanel.bottomAnchor, constant: -14).isActive = true
+        stackViewM.leadingAnchor.constraint(equalTo: registerPanel.centerXAnchor, constant: -30).isActive = true
+        stackViewM.centerYAnchor.constraint(equalTo: registerPanel.centerYAnchor).isActive = true
+        stackViewM.trailingAnchor.constraint(equalTo: registerPanel.trailingAnchor, constant: -14).isActive = true
+    }
+    
+}
